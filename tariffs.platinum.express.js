86a5d92582b72b179d0be3e402a065ff80ba8988
INGO <j67606724@gmail.com>
2025-12-09 23:01:48 +0300

    Add files via upload

---
+++ tariffs.platinum.express.js
@@ -0,0 +1,12 @@
+// tariffs.platinum.express.js
+const PRODUCT5_TARIFFS = [
+ {id:5, ke:0, oo:600000, di:0, go:200000, standard:9850, special:4925},
+ {id:6, ke:0, oo:700000, di:600000, go:300000, standard:7850, special:3925},
+ {id:7, ke:3000000, oo:550000, di:350000, go:300000, standard:6950, special:3475},
+ {id:8, ke:5000000, oo:600000, di:500000, go:600000, standard:8900, special:4450},
+ {id:9, ke:7000000, oo:800000, di:650000, go:900000, standard:10950, special:5475},
+ {id:10, ke:10000000, oo:1300000, di:1100000, go:1500000, standard:14900, special:7450},
+ {id:11, ke:11000000, oo:2500000, di:1500000, go:3000000, standard:22100, special:11050},
+ {id:14, ke:15000000, oo:3000000, di:2000000, go:3000000, standard:23100, special:null},
+ {id:15, ke:20000000, oo:5000000, di:4000000, go:5000000, standard:38500, special:null}
+];
