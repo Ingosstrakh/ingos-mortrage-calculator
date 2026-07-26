10094c4616362e1a0776a236928c4684a1d9cf07
INGO <j67606724@gmail.com>
2026-07-26 20:13:52 +0300

    Update tariffs.moyakvartira.js

--- tariffs.moyakvartira.js
+++ tariffs.moyakvartira.js
@@ -1,10 +1,9 @@
-// tariffs.moyakvartira.js
 export const T_MOYA = {
   constructive: { only: 0.001, withOthers: 0.0001 },
   finish: [
-    { min: 200000, max: 499999, rate: 0.0095 },  // До 499999 включительно
-    { min: 500000, max: 999999, rate: 0.0060 },  // От 500000 включительно до 999999 (500000 * 0.0060 = 3000)
-    { min: 1000000, max: 3000000, rate: 0.0045 },  // От 1000000 включительно до 3000000 (1000000 * 0.0045 = 4500)
+    { min: 200000, max: 499999, rate: 0.0095 },
+    { min: 500000, max: 999999, rate: 0.0060 },
+    { min: 1000000, max: 3000000, rate: 0.0045 },
     { min: 3000001, max: 5000000, rate: 0.0055 }
   ],
   movable: [
@@ -19,6 +18,6 @@ export const T_MOYA = {
       { min: 1000000, max: 5000000, rate: 0.0020 }
     ]
   },
-  risks: { gr1: 0.0020, gr23: 0.0015 }
+  risks: { gr1: 0.0020, gr23: 0.0015 },
+  warActions: { finish: 0.00055, movable: 0.00055 }
 };
-
